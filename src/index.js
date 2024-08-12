d2ef84c9d09084b3c6bab4359e62dab991fc6713
Alesia Via <alesiavia@outlook.com>
2024-08-12 07:00:01 +0300

    feat: check brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, bracketsConfig) {
+
+  
+  const bracketsPair = bracketsConfig.reduce((accum, item) => {
+    accum[item[1]] = item[0]
+    return accum
+  },
+    {}
+  );
+  const openBrackets = Object.values(bracketsPair);
+
+  function isBracketOk(str) {
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      
+       if (openBrackets.includes(currentSymbol) && bracketsPair[currentSymbol] === currentSymbol) {
+        if (stack.length > 0 && stack[stack.length - 1] === currentSymbol) {
+          stack.pop();  
+        } else {
+          stack.push(currentSymbol);  
+        }
+      } else if (openBrackets.includes(currentSymbol)) {
+      
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        let topElement = stack[stack.length - 1];
+        if (bracketsPair[currentSymbol] === topElement) {
+          stack.pop();
+        }
+        else {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0;
+  }
+
+  return isBracketOk(s)
+
 }
